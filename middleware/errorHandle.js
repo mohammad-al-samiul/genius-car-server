7573d6d7d333c764272330d5a0250490777e868f
Noob-H4cker <alsamiul123@gmail.com>
2024-01-04 19:51:20 +0600

    constants added

--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,5 +1,14 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            console.log("No Error!");
+            break;
+    }
     res.json({
         message : err.message,
         stackTrace : err.stack
