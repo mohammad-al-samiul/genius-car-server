b47ba15cb2e403e18d0bcd6d3b4ee39055e3fd75
Noob-H4cker <alsamiul123@gmail.com>
2024-01-04 20:00:33 +0600

    error handle added

--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,18 +1,49 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
-    switch (key) {
-        case value:
-            
-            break;
-    
-        default:
-            console.log("No Error!");
-            break;
-    }
-    res.json({
-        message : err.message,
-        stackTrace : err.stack
-    })
-}
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "validation failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "unauthorized access",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: "not found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "server error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+      console.log("No Error!");
+      break;
+  }
+
+};
 
 module.exports = errorHandler;
